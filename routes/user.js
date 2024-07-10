36c1f8fa163ebe00e42653cfe3e5ec81d1f59e49
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-07-10 08:27:40 -0500

    route update

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,12 @@ import * as userService from '../services/user.service'
 
 const userRoutes = express.Router();
 
-userRoutes.post("/",
-    catchAsync(userService.getAllUsers)
+const catchAsync = fn => (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+}
 
-)
+//create user
+userRoutes.post("/", catchAsync(async (req, res) => {
+    const user = await userService.createUser(req.body);
+    res.status(201).json(user)
+}))
