72d1cd58d579c6ccde0798361ba76f11b525894d
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-07-10 08:41:03 -0500

    update

--- routes/user.js
+++ routes/user.js
@@ -13,3 +13,17 @@ userRoutes.post("/", catchAsync(async (req, res) => {
     const user = await userService.createUser(req.body);
     res.status(201).json(user)
 }))
+
+// get All users
+
+userRoutes.get("/", catchAsync(async (req, res) => {
+    const user = await userService.getAllUsers(req.body);
+    res.status(200).json(user)
+}))
+
+// get user by id
+
+userRoutes.get("/", catchAsync(async (req, res) => {
+    const user = await userService.getUser(req.body);
+    res.status(200).json(user)
+}))
